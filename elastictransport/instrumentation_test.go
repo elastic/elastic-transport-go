20d3f87b27d0d81c71fff9906dd98499e4b0ff17
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2023-12-06 17:52:09 +0100

    Add tests

--- elastictransport/instrumentation_test.go
+++ elastictransport/instrumentation_test.go
@@ -19,12 +19,14 @@ package elastictransport
 
 import (
 	"context"
-	"go.opentelemetry.io/otel"
+	"fmt"
+	"go.opentelemetry.io/otel/codes"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	"go.opentelemetry.io/otel/trace"
 	"net/http"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -34,8 +36,7 @@ var endpoint = spanName
 func NewTestOpenTelemetry() (*tracetest.InMemoryExporter, *sdktrace.TracerProvider, *ElasticsearchOpenTelemetry) {
 	exporter := tracetest.NewInMemoryExporter()
 	provider := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter))
-	otel.SetTracerProvider(provider)
-	instrumentation := NewOtelInstrumentation(true, "8.99.0-SNAPSHOT")
+	instrumentation := NewOtelInstrumentation(provider, true, "8.99.0-SNAPSHOT")
 	return exporter, provider, instrumentation
 }
 
@@ -55,17 +56,17 @@ func TestElasticsearchOpenTelemetry_StartClose(t *testing.T) {
 		}
 
 		if len(exporter.GetSpans()) != 1 {
-			t.Fatalf("wrong number of spans recorded, got = %v, want %v", len(exporter.GetSpans()), 1)
+			t.Fatalf("wrong number of spans recorded, got %v, want %v", len(exporter.GetSpans()), 1)
 		}
 
 		span := exporter.GetSpans()[0]
 
 		if span.Name != spanName {
-			t.Errorf("Start() invalid span name, got = %v, want %v", span.Name, spanName)
+			t.Errorf("invalid span name, got %v, want %v", span.Name, spanName)
 		}
 
 		if span.SpanKind != trace.SpanKindClient {
-			t.Errorf("wrong Span kind, expected, got = %v, want %v", span.SpanKind, trace.SpanKindClient)
+			t.Errorf("wrong Span kind, expected, got %v, want %v", span.SpanKind, trace.SpanKindClient)
 		}
 	})
 }
@@ -75,10 +76,10 @@ func TestElasticsearchOpenTelemetry_BeforeRequest(t *testing.T) {
 		_, _, instrument := NewTestOpenTelemetry()
 
 		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "/", nil)
-		snapshot := req.Clone(context.Background())
 		if err != nil {
 			t.Fatalf("error while creating request")
 		}
+		snapshot := req.Clone(context.Background())
 		instrument.BeforeRequest(req, endpoint)
 
 		if !reflect.DeepEqual(req, snapshot) {
@@ -87,188 +88,247 @@ func TestElasticsearchOpenTelemetry_BeforeRequest(t *testing.T) {
 	})
 }
 
-//func TestElasticsearchOpenTelemetry_AfterRequest(t *testing.T) {
-//	type fields struct {
-//		tracer      trace.Tracer
-//		recordQuery bool
-//	}
-//	type args struct {
-//		req      *http.Request
-//		system   string
-//		endpoint string
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			i := &ElasticsearchOpenTelemetry{
-//				tracer:      tt.fields.tracer,
-//				recordQuery: tt.fields.recordQuery,
-//			}
-//			i.AfterRequest(tt.args.req, tt.args.system, tt.args.endpoint)
-//		})
-//	}
-//}
+func TestElasticsearchOpenTelemetry_AfterRequest(t *testing.T) {
+	t.Run("AfterRequest", func(t *testing.T) {
+		exporter, provider, instrument := NewTestOpenTelemetry()
+		fullUrl := "http://elastic:elastic@localhost:9200/test-index/_search"
 
-//func TestElasticsearchOpenTelemetry_RecordError(t *testing.T) {
-//	type fields struct {
-//		tracer      trace.Tracer
-//		recordQuery bool
-//	}
-//	type args struct {
-//		ctx context.Context
-//		err error
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			i := &ElasticsearchOpenTelemetry{
-//				tracer:      tt.fields.tracer,
-//				recordQuery: tt.fields.recordQuery,
-//			}
-//			i.RecordError(tt.args.ctx, tt.args.err)
-//		})
-//	}
-//}
-//
-//func TestElasticsearchOpenTelemetry_RecordClusterId(t *testing.T) {
-//	type fields struct {
-//		tracer      trace.Tracer
-//		recordQuery bool
-//	}
-//	type args struct {
-//		ctx context.Context
-//		id  string
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			i := &ElasticsearchOpenTelemetry{
-//				tracer:      tt.fields.tracer,
-//				recordQuery: tt.fields.recordQuery,
-//			}
-//			i.RecordClusterId(tt.args.ctx, tt.args.id)
-//		})
-//	}
-//}
-//
-//func TestElasticsearchOpenTelemetry_RecordNodeName(t *testing.T) {
-//	type fields struct {
-//		tracer      trace.Tracer
-//		recordQuery bool
-//	}
-//	type args struct {
-//		ctx  context.Context
-//		name string
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			i := &ElasticsearchOpenTelemetry{
-//				tracer:      tt.fields.tracer,
-//				recordQuery: tt.fields.recordQuery,
-//			}
-//			i.RecordNodeName(tt.args.ctx, tt.args.name)
-//		})
-//	}
-//}
-//
-//func TestElasticsearchOpenTelemetry_RecordPathPart(t *testing.T) {
-//	type fields struct {
-//		tracer      trace.Tracer
-//		recordQuery bool
-//	}
-//	type args struct {
-//		ctx      context.Context
-//		pathPart string
-//		value    string
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			i := &ElasticsearchOpenTelemetry{
-//				tracer:      tt.fields.tracer,
-//				recordQuery: tt.fields.recordQuery,
-//			}
-//			i.RecordPathPart(tt.args.ctx, tt.args.pathPart, tt.args.value)
-//		})
-//	}
-//}
-//
-//func TestElasticsearchOpenTelemetry_RecordQuery(t *testing.T) {
-//	type fields struct {
-//		tracer      trace.Tracer
-//		recordQuery bool
-//	}
-//	type args struct {
-//		ctx      context.Context
-//		endpoint string
-//		query    string
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			i := &ElasticsearchOpenTelemetry{
-//				tracer:      tt.fields.tracer,
-//				recordQuery: tt.fields.recordQuery,
-//			}
-//			i.RecordQuery(tt.args.ctx, tt.args.endpoint, tt.args.query)
-//		})
-//	}
-//}
-//
-//func TestNewOtelInstrumentation(t *testing.T) {
-//	type args struct {
-//		captureSearchBody bool
-//		version           string
-//	}
-//	tests := []struct {
-//		name string
-//		args args
-//		want *ElasticsearchOpenTelemetry
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			if got := NewOtelInstrumentation(tt.args.captureSearchBody, tt.args.version); !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("NewOtelInstrumentation() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+		ctx := instrument.Start(context.Background(), spanName)
+		req, err := http.NewRequestWithContext(ctx, http.MethodOptions, fullUrl, nil)
+		if err != nil {
+			t.Fatalf("error while creating request")
+		}
+		instrument.AfterRequest(req, "elasticsearch", endpoint)
+		instrument.Close(ctx)
+		err = provider.ForceFlush(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(exporter.GetSpans()) != 1 {
+			t.Fatalf("wrong number of spans recorded, got %v, want %v", len(exporter.GetSpans()), 1)
+		}
+
+		span := exporter.GetSpans()[0]
+
+		if span.Name != spanName {
+			t.Errorf("invalid span name, got %v, want %v", span.Name, spanName)
+		}
+
+		for _, attribute := range span.Attributes {
+			switch attribute.Key {
+			case attrDbSystem:
+				if !attribute.Valid() && attribute.Value.AsString() != "elasticsearch" {
+					t.Errorf("invalid %v, got %v, want %v", attrDbSystem, attribute.Value.AsString(), "elasticsearch")
+				}
+			case attrDbOperation:
+				if !attribute.Valid() && attribute.Value.AsString() != endpoint {
+					t.Errorf("invalid %v, got %v, want %v", attrDbOperation, attribute.Value.AsString(), endpoint)
+				}
+			case attrHttpRequestMethod:
+				if !attribute.Valid() && attribute.Value.AsString() != http.MethodOptions {
+					t.Errorf("invalid %v, got %v, want %v", attrHttpRequestMethod, attribute.Value.AsString(), http.MethodOptions)
+				}
+			case attrUrlFull:
+				if !attribute.Valid() && attribute.Value.AsString() != fullUrl {
+					t.Errorf("invalid %v, got %v, want %v", attrUrlFull, attribute.Value.AsString(), fullUrl)
+				}
+			case attrServerAddress:
+				if !attribute.Valid() && attribute.Value.AsString() != "localhost" {
+					t.Errorf("invalid %v, got %v, want %v", attrServerAddress, attribute.Value.AsString(), "localhost")
+				}
+			case attrServerPort:
+				if !attribute.Valid() && attribute.Value.AsInt64() != 9200 {
+					t.Errorf("invalid %v, got %v, want %v", attrServerPort, attribute.Value.AsInt64(), 9200)
+				}
+			}
+		}
+	})
+}
+
+func TestElasticsearchOpenTelemetry_RecordError(t *testing.T) {
+	exporter, provider, instrument := NewTestOpenTelemetry()
+
+	ctx := instrument.Start(context.Background(), spanName)
+	instrument.RecordError(ctx, fmt.Errorf("these are not the spans you are looking for"))
+	instrument.Close(ctx)
+	err := provider.ForceFlush(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(exporter.GetSpans()) != 1 {
+		t.Fatalf("wrong number of spans recorded, got %v, want %v", len(exporter.GetSpans()), 1)
+	}
+
+	span := exporter.GetSpans()[0]
+
+	if span.Name != spanName {
+		t.Errorf("invalid span name, got %v, want %v", span.Name, spanName)
+	}
+
+	if span.Status.Code != codes.Error {
+		t.Errorf("expected the span to have a status.code Error, got %v, want %v", span.Status.Code, codes.Error)
+	}
+}
+
+func TestElasticsearchOpenTelemetry_RecordClusterId(t *testing.T) {
+	exporter, provider, instrument := NewTestOpenTelemetry()
+
+	ctx := instrument.Start(context.Background(), spanName)
+	clusterId := "randomclusterid"
+	instrument.RecordClusterId(ctx, clusterId)
+	instrument.Close(ctx)
+	err := provider.ForceFlush(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(exporter.GetSpans()) != 1 {
+		t.Fatalf("wrong number of spans recorded, got %v, want %v", len(exporter.GetSpans()), 1)
+	}
+
+	span := exporter.GetSpans()[0]
+
+	if span.Name != spanName {
+		t.Errorf("invalid span name, got %v, want %v", span.Name, spanName)
+	}
+
+	for _, attribute := range span.Attributes {
+		switch attribute.Key {
+		case attrDbElasticsearchClusterName:
+			if !attribute.Valid() && attribute.Value.AsString() != clusterId {
+				t.Errorf("invalid %v, got %v, want %v", attrServerAddress, attribute.Value.AsString(), clusterId)
+			}
+		}
+	}
+}
+
+func TestElasticsearchOpenTelemetry_RecordNodeName(t *testing.T) {
+	exporter, provider, instrument := NewTestOpenTelemetry()
+
+	ctx := instrument.Start(context.Background(), spanName)
+	nodeName := "randomnodename"
+	instrument.RecordNodeName(ctx, nodeName)
+	instrument.Close(ctx)
+	err := provider.ForceFlush(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(exporter.GetSpans()) != 1 {
+		t.Fatalf("wrong number of spans recorded, got %v, want %v", len(exporter.GetSpans()), 1)
+	}
+
+	span := exporter.GetSpans()[0]
+
+	if span.Name != spanName {
+		t.Errorf("invalid span name, got %v, want %v", span.Name, spanName)
+	}
+
+	for _, attribute := range span.Attributes {
+		switch attribute.Key {
+		case attrDbElasticsearchNodeName:
+			if !attribute.Valid() && attribute.Value.AsString() != nodeName {
+				t.Errorf("invalid %v, got %v, want %v", attrDbElasticsearchNodeName, attribute.Value.AsString(), nodeName)
+			}
+		}
+	}
+}
+
+func TestElasticsearchOpenTelemetry_RecordPathPart(t *testing.T) {
+	exporter, provider, instrument := NewTestOpenTelemetry()
+	indexName := "test-index"
+	pretty := "true"
+
+	ctx := instrument.Start(context.Background(), spanName)
+	instrument.RecordPathPart(ctx, "index", indexName)
+	instrument.RecordPathPart(ctx, "pretty", pretty)
+	instrument.Close(ctx)
+	err := provider.ForceFlush(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(exporter.GetSpans()) != 1 {
+		t.Fatalf("wrong number of spans recorded, got %v, want %v", len(exporter.GetSpans()), 1)
+	}
+
+	span := exporter.GetSpans()[0]
+
+	if span.Name != spanName {
+		t.Errorf("invalid span name, got %v, want %v", span.Name, spanName)
+	}
+
+	for _, attribute := range span.Attributes {
+		switch attribute.Key {
+		case attrPathParts + "index":
+			if !attribute.Valid() && attribute.Value.AsString() != indexName {
+				t.Errorf("invalid %v, got %v, want %v", attrPathParts+"index", attribute.Value.AsString(), indexName)
+			}
+		case attrPathParts + "pretty":
+			if !attribute.Valid() && attribute.Value.AsString() != indexName {
+				t.Errorf("invalid %v, got %v, want %v", attrPathParts+"pretty", attribute.Value.AsString(), indexName)
+			}
+		}
+	}
+}
+
+func TestElasticsearchOpenTelemetry_RecordQuery(t *testing.T) {
+	exporter, provider, instrument := NewTestOpenTelemetry()
+	fullUrl := "http://elastic:elastic@localhost:9200/test-index/_search"
+	query := `{"query": {"match_all": {}}}`
+
+	// Won't log query
+	ctx := instrument.Start(context.Background(), spanName)
+	_, err := http.NewRequestWithContext(ctx, http.MethodOptions, fullUrl, strings.NewReader(query))
+	if err != nil {
+		t.Fatalf("error while creating request")
+	}
+	instrument.RecordQuery(ctx, "random", query)
+	instrument.Close(ctx)
+
+	// Will log query
+	secondCtx := instrument.Start(context.Background(), spanName)
+	_, err = http.NewRequestWithContext(ctx, http.MethodOptions, fullUrl, strings.NewReader(query))
+	if err != nil {
+		t.Fatalf("error while creating request")
+	}
+	instrument.RecordQuery(secondCtx, endpoint, query)
+	instrument.Close(secondCtx)
+
+	err = provider.ForceFlush(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(exporter.GetSpans()) != 2 {
+		t.Fatalf("wrong number of spans recorded, got %v, want %v", len(exporter.GetSpans()), 1)
+	}
+
+	span := exporter.GetSpans()[0]
+	if span.Name != spanName {
+		t.Errorf("invalid span name, got %v, want %v", span.Name, spanName)
+	}
+
+	for _, attribute := range span.Attributes {
+		switch attribute.Key {
+		case attrDbStatement:
+			t.Errorf("span should not have a %v entry", attrDbStatement)
+		}
+	}
+
+	querySpan := exporter.GetSpans()[1]
+	if querySpan.Name != spanName {
+		t.Errorf("invalid span name, got %v, want %v", querySpan.Name, spanName)
+	}
+
+	for _, attribute := range querySpan.Attributes {
+		switch attribute.Key {
+		case attrDbStatement:
+			if !attribute.Valid() && attribute.Value.AsString() != query {
+				t.Errorf("invalid query provided, got %v, want %v", attribute.Value.AsString(), query)
+			}
+		}
+	}
+}
