6a613f1f40bb1836632d90258a1dc3f9fce48ca7
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2025-04-07 17:44:56 +0200

    feat: add context support to DiscoverNodes and getNodesInfo methods (#29)

--- elastictransport/elastictransport.go
+++ elastictransport/elastictransport.go
@@ -20,6 +20,7 @@ package elastictransport
 import (
 	"bytes"
 	"compress/gzip"
+	"context"
 	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
@@ -105,6 +106,7 @@ type Config struct {
 	ConnectionPoolFunc func([]*Connection, Selector) ConnectionPool
 
 	CertificateFingerprint string
+	DiscoveryContext       context.Context
 }
 
 // Client represents the HTTP client.
@@ -264,7 +266,7 @@ func New(cfg Config) (*Client, error) {
 
 	if client.discoverNodesInterval > 0 {
 		time.AfterFunc(client.discoverNodesInterval, func() {
-			client.scheduleDiscoverNodes(client.discoverNodesInterval)
+			client.scheduleDiscoverNodes(cfg.DiscoveryContext, client.discoverNodesInterval)
 		})
 	}
 
